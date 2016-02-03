f97ec500ef56f7a3b4e2b03ea7257bfa62e7500a
kyoungae <tomi507@naver.com>
2016-02-03 13:30:27 +0900

    android project

---
+++ app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kka.myfirstandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //부모한테 가져온거고 밑은 추가로 할꺼
+        //화면에 레이아웃 표시 -  부모가 준거
+        // R 이 res 폴더 가리킴
+        setContentView(R.layout.activity_main);
+    }
+}
