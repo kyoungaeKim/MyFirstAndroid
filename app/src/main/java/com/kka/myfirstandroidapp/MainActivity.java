47653377bf726a0adf802f3689a0e232685e7e91
kyoungae <tomi507@naver.com>
2016-02-04 14:36:01 +0900

    Button click event log.d

--- app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
@@ -2,8 +2,12 @@ package com.kka.myfirstandroidapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         //부모한테 가져온거고 밑은 추가로 할꺼
         //화면에 레이아웃 표시 -  부모가 준거
         // R 이 res 폴더 가리킴
-        setContentView(R.layout.item_list2);
+        setContentView(R.layout.item_button);
+    }
+
+    // item_button.xml에 연결됨
+    public void onClick(View view) {
+        Log.d(TAG, "클릭 잘 됨");
     }
 }
