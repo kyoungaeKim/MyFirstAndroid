b29911aeee044aabf7aa351da43e8374c346356c
kyoungae <tomi507@naver.com>
2016-02-04 14:51:33 +0900

    Button click event onClickListener

--- app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         //화면에 레이아웃 표시 -  부모가 준거
         // R 이 res 폴더 가리킴
         setContentView(R.layout.item_button);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "클릭 완전 잘 됨", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(listener);
     }
 
     // item_button.xml에 연결됨
     public void onClick(View view) {
         Log.d(TAG, "클릭 잘 됨");
+        Toast.makeText(MainActivity.this, "클릭 잘 됨", Toast.LENGTH_SHORT).show();
     }
 }
