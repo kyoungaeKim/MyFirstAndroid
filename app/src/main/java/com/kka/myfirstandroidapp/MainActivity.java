6bc1c642cb379f71eb9be23cf868542a61004642
kyoungae <tomi507@naver.com>
2016-02-04 13:37:31 +0900

    chapter_1

--- app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.kka.myfirstandroidapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         //부모한테 가져온거고 밑은 추가로 할꺼
         //화면에 레이아웃 표시 -  부모가 준거
         // R 이 res 폴더 가리킴
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.item_list2);
     }
 }
