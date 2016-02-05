a660d2726816625ad45d9ed740d6a369de88cfa7
kyoungae <tomi507@naver.com>
2016-02-05 10:35:08 +0900

    visibility 속성 변경

---
+++ app/src/main/java/com/kka/myfirstandroidapp/AT.java
@@ -0,0 +1,7 @@
+package com.kka.myfirstandroidapp;
+
+/**
+ * Created by user on 2016-02-05.
+ */
+public class AT {
+}
--- app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/kka/myfirstandroidapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.kka.myfirstandroidapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //부모한테 가져온거고 밑은 추가로 할꺼
         //화면에 레이아웃 표시 -  부모가 준거
         // R 이 res 폴더 가리킴
-        setContentView(R.layout.item_button);
+        setContentView(R.layout.check);
+        mTextView = (TextView) findViewById(R.id.msg_text_view);
+        ((CheckBox) findViewById(R.id.check_box)).setOnCheckedChangeListener(this);
+
 
 //        Button button2 = (Button) findViewById(R.id.button2);
 //        Button button1 = (Button) findViewById(R.id.button1);
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
+//        findViewById(R.id.button1).setOnClickListener(this);
+//        findViewById(R.id.button2).setOnClickListener(this);
 //        button2.setOnClickListener(this);
 //        button1.setOnClickListener(this);
 
@@ -47,14 +54,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // item_button.xml에 연결됨
     public void onClick(View v) {
-        //view v 는 다형성으로 버튼 받은거
-        switch (v.getId()) {
-            case R.id.button1:
-                Toast.makeText(MainActivity.this, "첫번째 버튼", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "두우번째 버튼", Toast.LENGTH_SHORT).show();
-                break;
+//        //view v 는 다형성으로 버튼 받은거
+//        switch (v.getId()) {
+//            case R.id.button1:
+//                Toast.makeText(MainActivity.this, "첫번째 버튼", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button2:
+//                Toast.makeText(MainActivity.this, "두우번째 버튼", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+    }
+
+    //체크박스가 들어옴
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Toast.makeText(MainActivity.this, "check : isChecked", Toast.LENGTH_SHORT).show();
+        if (isChecked) {
+            mTextView.setVisibility(View.VISIBLE);
+        } else {
+            mTextView.setVisibility(View.INVISIBLE);
         }
+
     }
 }
